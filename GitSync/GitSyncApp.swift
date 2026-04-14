2d30ba1e90366de73bcc17ec179e64481b238071
Jude <lehyungju@gmail.com>
2026-04-14 16:57:53 +0900

    Initial Commit

---
+++ GitSync/GitSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSyncApp.swift
+//  GitSync
+//
+//  Created by Jude on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct GitSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
